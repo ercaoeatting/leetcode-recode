e90c17bb32f7cff52b2ef24da5f9a2675b8107c0
L-H <1129339030@qq.com>
2024-03-05 22:21:18 +0800

    3.5 over

--- _7backtracking.cpp
+++ _7backtracking.cpp
@@ -132,3 +132,132 @@ public:
         return result;
     }
 };
+// 分割问题
+class lc131 {
+    vector<vector<string>> result;
+    vector<string> str;
+
+public:
+    bool ifretstr(string s) {
+        int left = 0, right = s.size() - 1;
+        while (left <= right) {
+            if (s[left++] != s[right--]) return false;
+        }
+        return true;
+    }
+    void backtracking(string s) {
+        if (1 > s.size()) {
+            result.push_back(str);
+            return;
+        }
+        for (int i = 1; i <= s.size(); i++) {
+            string subs = string(s.begin(), s.begin() + i);
+            if (ifretstr(subs)) {
+                str.push_back(subs);
+                backtracking(string(s.begin() + i, s.end()));
+                str.pop_back();
+            }
+            else { continue; }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        if (s.size() == 0) return result;
+        backtracking(s);
+        return result;
+    }
+};
+class lc131_sol2 {
+private:
+    vector<vector<string>> result;
+    vector<string> path;               // 放已经回文的子串
+    vector<vector<bool>> isPalindrome; // 放事先计算好的是否回文子串的结果
+    void backtracking(const string& s, int startIndex) {
+        // 如果起始位置已经大于s的大小，说明已经找到了一组分割方案了
+        if (startIndex >= s.size()) {
+            result.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i < s.size(); i++) {
+            if (isPalindrome[startIndex][i]) { // 是回文子串
+                // 获取[startIndex,i]在s中的子串
+                string str = s.substr(startIndex, i - startIndex + 1);
+                path.push_back(str);
+            }
+            else { // 不是回文，跳过
+                continue;
+            }
+            backtracking(s, i + 1); // 寻找i+1为起始位置的子串
+            path.pop_back();        // 回溯过程，弹出本次已经添加的子串
+        }
+    }
+    void computePalindrome(const string& s) {
+        // isPalindrome[i][j] 代表 s[i:j](双边包括)是否是回文字串
+        isPalindrome.resize(s.size(),
+                            vector<bool>(s.size(), false)); // 根据字符串s, 刷新布尔矩阵的大小
+        for (int i = s.size() - 1; i >= 0; i--) {
+            // 需要倒序计算, 保证在i行时, i+1行已经计算好了
+            for (int j = i; j < s.size(); j++) {
+                if (j == i) { isPalindrome[i][j] = true; }
+                else if (j - i == 1) { isPalindrome[i][j] = (s[i] == s[j]); }
+                else { isPalindrome[i][j] = (s[i] == s[j] && isPalindrome[i + 1][j - 1]); }
+            }
+        }
+    }
+
+public:
+    vector<vector<string>> partition(string s) {
+        result.clear();
+        path.clear();
+        computePalindrome(s);
+        backtracking(s, 0);
+        return result;
+    }
+};
+
+// 复原IP地址
+class lc93 {
+    vector<string> result;
+    bool isValid(const string& s, int start, int end) {
+        if (start > end) { return false; }
+        if (s[start] == '0' && start != end) { // 0开头的数字不合法
+            return false;
+        }
+        int num = 0;
+        for (int i = start; i <= end; i++) {
+            if (s[i] > '9' || s[i] < '0') { // 遇到非数字字符不合法
+                return false;
+            }
+            num = num * 10 + (s[i] - '0');
+            if (num > 255) { // 如果大于255了不合法
+                return false;
+            }
+        }
+        return true;
+    }
+    void backtracking(string& s, int startIndex, int point) {
+        if (point == 3) {
+            if (isValid(s, startIndex, s.size() - 1)) {
+                result.push_back(s);
+                return;
+            }
+        }
+        for (int i = startIndex; i < s.size(); i++) {
+            if (isValid(s, startIndex, i)) {
+                s.insert(s.begin() + i + 1, '.');
+                backtracking(s, i + 2, point + 1);
+                s.erase(s.begin() + i + 1);
+            }
+            else
+                break;
+        }
+    }
+
+public:
+    vector<string> restoreIpAddresses(string s) {
+        if (s.size() < 4 || s.size() > 12) return result;
+        backtracking(s, 0, 0);
+        return result;
+    }
+};
+
+int main() {}
