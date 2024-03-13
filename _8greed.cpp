9e2315361ee7a5276f8b0d0f170bdbe996cb99e7
L-H <1129339030@qq.com>
2024-03-13 20:02:30 +0800

    3.13

--- _8greed.cpp
+++ _8greed.cpp
@@ -35,3 +35,41 @@ public:
         return count;
     }
 };
+// 最大子序和
+class lc53 {
+public:
+    // 贪心：当前“连续和”为负数的时候立刻放弃，从下一个元素重新计算“连续和”
+    int maxSubArray(vector<int>& nums) {
+        int result = INT32_MIN;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            count += nums[i];
+            if (count > result) { // 取区间累计的最大值（相当于不断确定最大子序终止位置）
+                result = count;
+            }
+            if (count <= 0) count = 0; // 相当于重置最大子序起始位置，因为遇到负数一定是拉低总和
+        }
+        return result;
+    }
+};
+// 买卖股票的最佳时机II
+class lc122 {
+public:
+    // 局部最优：收集每天的正利润
+    int maxProfit(vector<int>& prices) {
+        int result = 0;
+        for (int i = 1; i < prices.size(); i++) { result += max(0, prices[i] - prices[i - 1]); }
+        return result;
+    }
+};
+// 跳跃游戏
+class lc55 {
+public:
+    bool canJump(vector<int>& nums) {
+        int cover = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (cover >= i) cover = max(cover, i + nums[i]);
+        }
+        return cover > nums.size() - 1;
+    }
+};
