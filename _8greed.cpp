669feb82d279ad2c39e823d4bab0a15e8f9cc2db
L-H <1129339030@qq.com>
2024-03-19 17:33:28 +0800

    test

--- _8greed.cpp
+++ _8greed.cpp
@@ -242,4 +242,42 @@ public:
     }
 };
 
-int main() {}
+<<<<<<< HEAD
+int main(){} == == == = public : int findMinArrowShots(vector<vector<int>> & points) {
+    if (points.size() == 0) return 0;
+    sort(points.begin(), points.end(), cmp);
+
+    int result = 1; // points 不为空至少需要一支箭
+    for (int i = 1; i < points.size(); i++) {
+        if (points[i][0] > points[i - 1][1]) { // 气球i和气球i-1不挨着，注意这里不是>=
+            result++;                          // 需要一支箭
+        }
+        else {                                                  // 气球i和气球i-1挨着
+            points[i][1] = min(points[i - 1][1], points[i][1]); // 更新重叠气球最小右边界
+        }
+    }
+    return result;
+}
+}
+;
+
+// 无重叠区间
+class lc435 {
+public:
+    // 按照区间右边界排序
+    static bool cmp(const vector<int>& a, const vector<int>& b) { return a[1] < b[1]; }
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if (intervals.size() == 0) return 0;
+        sort(intervals.begin(), intervals.end(), cmp);
+        int count = 1;             // 记录非交叉区间的个数
+        int end = intervals[0][1]; // 记录区间分割点
+        for (int i = 1; i < intervals.size(); i++) {
+            if (end <= intervals[i][0]) {
+                end = intervals[i][1];
+                count++;
+            }
+        }
+        return intervals.size() - count;
+    }
+};
+/// afasf
