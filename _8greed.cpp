4f3836d1b7d32dde76ea26a8875d6a8380d89d19
L-H <1129339030@qq.com>
2024-03-22 23:23:14 +0800

    3.22

--- _8greed.cpp
+++ _8greed.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cstdint>
 #include <cstdlib>
+#include <iostream>
 #include <list>
 #include <string>
 #include <vector>
@@ -476,8 +477,78 @@ public:
         vector<int> dp(n + 1);
         dp[0] = 0;
         dp[1] = 1;
+        if (n < 2) return n; // 防空指针，也是剪枝
         for (int i = 2; i <= n; i++) { dp[i] = dp[i - 1] + dp[i - 2]; }
         return dp[n];
     }
 };
-int main() { int n = 10; }
+class lc70 {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1);
+        if (n <= 2) return n;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) { dp[i] = dp[i - 1] + dp[i - 2]; }
+        return dp[n];
+    }
+    // 依然可以优化空间复杂度
+    class Solution {
+    public:
+        int climbStairs(int n) {
+            if (n <= 1) return n;
+            int dp[3];
+            dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i <= n; i++) {
+                int sum = dp[1] + dp[2];
+                dp[1] = dp[2];
+                dp[2] = sum;
+            }
+            return dp[2];
+        }
+    };
+};
+class kama_57 {
+public:
+    int goupstairs(int n, int m) {
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) { // 把m换成2，就可以AC爬楼梯这道题
+                if (i - j >= 0) dp[i] += dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
+class lc746 {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        if (cost.size() == 1) return cost[0];
+        vector<int> dp(cost.size() + 1);
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <= cost.size(); i++) {
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[cost.size()];
+    }
+};
+// 不同路径
+class lc62 {
+public:
+    int uniquePaths(int m, int n) {
+        int dp[m][n];
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int i = 0; i < n; i++) dp[0][i] = 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) { dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+int main() {
+    int m, n;
+    cin >> m >> n;
+}
